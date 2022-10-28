332052c4b79df50b99a64d0d75fae8afe1d6447c
yosida_masaya <sasaki123.321ikasas@gmail.com>
2022-10-28 23:05:39 +0900

    メモ一蘭の取得

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,16 +29,37 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //ログインしているユーザー情報取得
+        $user = Auth::user();
+        //メモ一覧を取得
+        //where()の第一引数に絞り込み項目
+        //第二引数に絞り込みに使用する値
+        //whereを2つ繋げて「&&」の意味
+        $memos = Memo::where('user_id', $user['id'])
+        ->where('status', 1)
+        ->orderBy('updated_at', 'DESC')
+        ->get();
+        return view('home', ['memos' => $memos]);
     }
 
     public function create()
     {
-        //Auht::user()で現在認証しているユーザーを取得
+        //ログインしているユーザー情報取得
         $user = Auth::user();
+        //メモ一覧を取得
+        //where()の第一引数に絞り込み項目
+        //第二引数に絞り込みに使用する値
+        //whereを2つ繋げて「&&」の意味
+        $memos = Memo::where('user_id', $user['id'])
+        ->where('status', 1)
+        //orderBy()で並べ方の指定
+        //ASC=昇順、DESC=降順
+        ->orderBy('updated_at', 'DESC')
+        ->get();
+
         return view(
             'create',
-            compact('user')
+            ['memos' => $memos, 'user' => $user]
         );
     }
 
@@ -50,20 +71,20 @@ class HomeController extends Controller
         // MEMOモデルにDBへ保存する命令を出す
 
         // 同じタグがあるか確認
-        $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
-        if( empty($exist_tag['id']) ){
+        //$exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
+        //if( empty($exist_tag['id']) ){
             //先にタグをインサート
-            $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
-        }else{
-            $tag_id = $exist_tag['id'];
-        }
+            //$tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+        //}else{
+            //$tag_id = $exist_tag['id'];
+        //}
 
         //タグのIDが判明する
         // タグIDをmemosテーブルに入れてあげる
         $memo_id = Memo::insertGetId([
             'content' => $data['content'],
              'user_id' => $data['user_id'], 
-             'tag_id' => $tag_id,
+             //'tag_id' => $tag_id,
              'status' => 1
         ]);
         
